211564c67301bb6549b325349043bd7ac47e4eaf
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-08-22 13:28:18 -0400

    Full Velocity Port

---
+++ src/main/java/de/simonsator/partyandfriends/velocity/maxfriends/MFConfig.java
@@ -0,0 +1,30 @@
+package de.simonsator.partyandfriends.velocity.maxfriends;
+
+
+
+import de.simonsator.partyandfriends.velocity.api.PAFExtension;
+import de.simonsator.partyandfriends.velocity.utilities.ConfigurationCreator;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MFConfig extends ConfigurationCreator {
+	protected MFConfig(File file, PAFExtension pPlugin) throws IOException {
+		super(file, pPlugin, true);
+		copyFromJar();
+		readFile();
+		loadDefaultValues();
+		saveFile();
+		process();
+	}
+
+	private void loadDefaultValues() {
+		set("General.DefaultMaxFriends", 10);
+		set("General.AddMaxFriendsPermission", "partyandfriends.maxfriends.10|10",
+				"partyandfriends.maxfriends.20|20", "partyandfriends.maxfriends.50|50");
+		set("General.UnlimitedFriendsPermission", "partyandfriends.maxfriends.unlimited");
+		set("Messages.YouTooManyFriends", " &7You already have too many Friends. Too add new Friends you need to delete old once.");
+		set("Messages.OtherTooManyFriends", " &7The requested person already has too many Friends. " +
+				"He needs first to delete some of his old friends so you can add him as a friend.");
+	}
+}
